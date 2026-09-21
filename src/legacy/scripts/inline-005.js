d59a593b6a104874b31b4f794dd60e35f303fc10
tevidentkemp-code <tevidentkemp@gmail.com>
2026-09-21 01:21:55 +0100

    SC-053: rematerialize canonical semantic JS runtime

--- src/legacy/scripts/inline-005.js
+++ src/legacy/scripts/inline-005.js
@@ -14258,7 +14258,7 @@ function buildPad(){
         (typeof state !== 'undefined' && state && typeof state.currentDartIndex === 'number') ? state.currentDartIndex :
         (typeof state !== 'undefined' && state && typeof state.dartIndex === 'number') ? state.dartIndex : 0;
 
-      const specialMissSeq = (dart >= 0 && dart <= 2 && n >= 2);
+      const specialMissSeq = (dart >= 0 && dart <= 2 && n >= 1);
       const seqSlots = [' ', ' ', ' '];
 
       try{
@@ -14310,9 +14310,9 @@ function buildPad(){
           base[idx] = 'X';
           try{
             const z3 = `${base[0]} / ${base[1]} / ${base[2]}`;
-            window.sqDmdShowZones?.({ z2: label, z3, z3Small:true }, { type:'snap', ms:150, revealMs:125, amp:2.6, fx:'impact' });
+            window.sqDmdShowZones?.({ z2: label, z3, z3Small:true }, { type:'flash', ms:260, fx:'impact' });
           }catch(_){}
-          await new Promise(r=>setTimeout(r,165));
+          await new Promise(r=>setTimeout(r,280));
         }
       }
 
@@ -14378,7 +14378,7 @@ function buildPad(){
           (typeof state !== 'undefined' && state && typeof state.currentDartIndex === 'number') ? state.currentDartIndex :
           (typeof state !== 'undefined' && state && typeof state.dartIndex === 'number') ? state.dartIndex : 0;
 
-        const specialMissSeq = (dart >= 0 && dart <= 2 && n >= 2);
+        const specialMissSeq = (dart >= 0 && dart <= 2 && n >= 1);
         const seqSlots = [' ', ' ', ' '];
 
         try{
@@ -14431,9 +14431,9 @@ function buildPad(){
             base[idx] = 'X';
             try{
               const z3 = `${base[0]} / ${base[1]} / ${base[2]}`;
-              window.sqDmdShowZones?.({ z2: label, z3, z3Small:true }, { type:'snap', ms:150, revealMs:125, amp:2.6, fx:'impact' });
+              window.sqDmdShowZones?.({ z2: label, z3, z3Small:true }, { type:'flash', ms:260, fx:'impact' });
             }catch(_){}
-            await new Promise(r=>setTimeout(r,165));
+            await new Promise(r=>setTimeout(r,280));
           }
         }
 
